55799def6d44a37b55ed72ff1c7c412c7fe4501b
tgoncalves07 <84681357+tgoncalves07@users.noreply.github.com>
2025-06-16 16:36:51 +0100

    v.1.0.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			typescript: true
-		})
-	]
+	kit: {
+		adapter: adapter({})
+	},
+	preprocess: [vitePreprocess()]
 };
